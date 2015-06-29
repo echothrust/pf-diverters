8308f373253be4dd352be4ced44f89d98a99c3ef
Pantelis Roditis <proditis@echothrust.com>
2015-06-29 12:30:27 +0300

    style rule "Prototypes should not have variable names associated with the types"

--- daemon.h
+++ daemon.h
@@ -1,6 +1,6 @@
 #ifndef DAEMON_H
 #define DAEMON_H 1
 void daemonShutdown();
-void signal_handler(int sig);
-void daemonize(char *rundir, char *pidfile);
+void signal_handler(int);
+void daemonize(char *, char *);
 #endif
